ba05293b047a7c7912a7d8de4c767acbab7bc96d
Jromer94 <Jromer94@gmail.com>
2014-10-28 18:51:18 -0400

    added checkbox to choose persistence

--- menu.js
+++ menu.js
@@ -15,14 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
 		title = title.value;
 		var favicon = document.getElementById('titleform').elements['favicon'];
 		favicon = favicon.value;
+		var checkbox  = document.getElementById('titleform').elements['check'];
 		chrome.tabs.executeScript({code:"document.title = '" + title +"'"});
 		
 		chrome.tabs.executeScript({code:"console.log('hello'); var target = document.getElementsByTagName('title')[0]; console.log('hello'); var observer = new window.WebKitMutationObserver(function(mutations) {document.title='" + title + "'; console.log('changed title'); }); console.log('got here'); var config = {attributes:true, childList: true, characterData: true, subtree: true}; observer.observe(target, config);"});	
 
 		chrome.tabs.executeScript({code:"var link = document.createElement('link'); link.type = 'image/x-icon'; link.rel = 'shortcut icon'; link.href = '" + favicon + "'; document.getElementsByTagName('head')[0].insertBefore(link ,document.getElementsByTagName('head')[0].firstChild);"});
 		//doesn't work right now		
-		chrome.runtime.sendMessage({tab:currTab, title:title, favicon:favicon});
-	
+		if(checkbox.checked){
+			chrome.runtime.sendMessage({tab:currTab, title:title, favicon:favicon});
+		}
 	});
 });
 
