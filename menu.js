1f2b3d54f38665273f2d0121ecf89cc3616d38ec
Jromer94 <Jromer94@gmail.com>
2014-10-30 23:58:23 -0400

    almost have remove working

--- menu.js
+++ menu.js
@@ -5,27 +5,34 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
 		currTab = tabs[0].id;
-	
+			
+		var button = document.getElementById('submit');
+		var title = document.getElementById('titleform').elements['title'];
+		var favicon = document.getElementById('titleform').elements['favicon'];
+		var checkbox  = document.getElementById('titleform').elements['check'];
+
 		console.log(currTab);
 		chrome.runtime.sendMessage(currTab, function(response) {
 			console.log(response);
+			if(response){
+				title.disabled = true;
+				favicon.disabled = true;	
+				button.innerHTML = "Remove";
+			}
 		});	
 		var test = chrome.extension.getBackgroundPage();
 		console.log(test);
-		var button = document.getElementById('submit');
 		button.addEventListener("click", function(){
 
-			var title = document.getElementById('titleform').elements['title'];
-			title = title.value;
-			var favicon = document.getElementById('titleform').elements['favicon'];
-			favicon = favicon.value;
-			var checkbox  = document.getElementById('titleform').elements['check'];
-			chrome.tabs.executeScript({code:"document.title = '" + title +"'"});
-			
-			chrome.tabs.executeScript({code:"console.log('hello'); var target = document.getElementsByTagName('title')[0]; console.log('hello'); var observer = new window.WebKitMutationObserver(function(mutations) {document.title='" + title + "'; console.log('changed title'); }); console.log('got here'); var config = {attributes:true, childList: true, characterData: true, subtree: true}; observer.observe(target, config);"});	
+			if(!title.disabled){
+				title = title.value;
+				favicon = favicon.value;
+				chrome.tabs.executeScript({code:"document.title = '" + title +"'"});
+				
+				chrome.tabs.executeScript({code:"console.log('hello'); var target = document.getElementsByTagName('title')[0]; console.log('hello'); var observer = new window.WebKitMutationObserver(function(mutations) {document.title='" + title + "'; console.log('changed title'); }); console.log('got here'); var config = {attributes:true, childList: true, characterData: true, subtree: true}; observer.observe(target, config);"});	
 
-			chrome.tabs.executeScript({code:"var link = document.createElement('link'); link.type = 'image/x-icon'; link.rel = 'shortcut icon'; link.href = '" + favicon + "'; document.getElementsByTagName('head')[0].insertBefore(link ,document.getElementsByTagName('head')[0].firstChild);"});
-
+				chrome.tabs.executeScript({code:"var link = document.createElement('link'); link.type = 'image/x-icon'; link.rel = 'shortcut icon'; link.href = '" + favicon + "'; document.getElementsByTagName('head')[0].insertBefore(link ,document.getElementsByTagName('head')[0].firstChild);"});
+			}
 			if(checkbox.checked){
 				chrome.runtime.sendMessage({tab:currTab, title:title, favicon:favicon});
 			}
